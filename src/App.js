9b4bc4643b87cfdb470e2a83aee19b722f8f7d7f
이재협 <zzok3312@naver.com>
2024-04-09 23:33:36 +0900

    feat: react 설치

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <div style={{ fontSize: 30, fontWeight: 600 }}>
+        GitHub Action으로 CI/CD 구축해보기
+      </div>
+    </div>
+  );
+}
+
+export default App;
